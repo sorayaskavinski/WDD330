78e580493f696011a2f2f406906078c3c3b4d490
soraya-skavinski-thaddeus <sorayaskavinski@thaddeus.org>
2025-05-23 18:42:53 -0300

    dynamic details

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -30,11 +30,11 @@ export function getParam(param) {
   return product
 }
 
-export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
+export function renderListWithTemplate(template, parentElement, list, position = 'afterbegin', clear = false) {
   const htmlStrings = list.map(template);
   // if clear is true we need to clear out the contents of the parent.
   if (clear) {
     parentElement.innerHTML = '';
   }
-  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(''));
 }
